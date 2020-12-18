9afea155b9c2303be0abf0910675709b92a8ebe6
danamurphy48 <danamurphy48@gmail.com>
2020-12-18 12:45:59 -0600

    Added transport options list

--- DayTripGenerator/Transportation.cs
+++ DayTripGenerator/Transportation.cs
@@ -6,5 +6,22 @@ namespace DayTripGenerator
 {
     class Transportation
     {
+        //member variables
+        public List<string> transportOption;
+        public Transportation()
+        {
+            transportOption = new List<string>();
+        }
+
+        //member methods
+        public void OptionTransport()
+        {
+            transportOption.Add("Car");
+            transportOption.Add("Plane");
+            transportOption.Add("Train");
+            transportOption.Add("Boat");
+            transportOption.Add("Bicycle");
+            transportOption.Add("Walk");
+        }
     }
 }
