65ca712b975b508499eabcf5f616f1eb85ab0730
Dana Murphy <danamurphy48@gmail.com>
2021-02-17 18:25:17 -0600

    Added options to Destination class

--- DayTripGenerator/Destination.cs
+++ DayTripGenerator/Destination.cs
@@ -6,5 +6,24 @@ namespace DayTripGenerator
 {
     class Destination
     {
+        //member variables
+        public List<string> locationOption;
+
+        //constructor
+        public Destination()
+        {
+            locationOption = new List<string> ();
+        }
+
+        //member methods
+        public void OptionLocation()
+        {
+            locationOption.Add("Milwaukee");
+            locationOption.Add("Madison");
+            locationOption.Add("Chicago");
+            locationOption.Add("Green Bay");
+            locationOption.Add("Minneapolis");
+            locationOption.Add("Rockford");
+        }
     }
 }
