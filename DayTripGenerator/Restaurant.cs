c5334c8e56b957bb6f66967bdf6123ead73db86f
Dana Murphy <danamurphy48@gmail.com>
2021-02-26 11:27:08 -0600

    Added restaurant options

--- DayTripGenerator/Restaurant.cs
+++ DayTripGenerator/Restaurant.cs
@@ -6,5 +6,31 @@ namespace DayTripGenerator
 {
     class Restaurant
     {
+        //member variables (has a)
+        public List<string> restaurantOptions;
+        public Restaurant()
+        {
+            restaurantOptions = new List<string>();
+        }
+
+        //member methods (can do)
+        public void AddRestaurantOptions()
+        {
+            restaurantOptions.Add("Italian");
+            restaurantOptions.Add("French");
+            restaurantOptions.Add("Portugese");
+            restaurantOptions.Add("Steak");
+            restaurantOptions.Add("American");
+            restaurantOptions.Add("Brazilian");
+            restaurantOptions.Add("Spanish");
+            restaurantOptions.Add("Irish");
+            restaurantOptions.Add("Indian");
+            restaurantOptions.Add("Chinese");
+            restaurantOptions.Add("Nepalese");
+            restaurantOptions.Add("Vietnamese");
+            restaurantOptions.Add("Thai");
+            restaurantOptions.Add("Japanese");
+            restaurantOptions.Add("Swedish");
+        }
     }
 }
