00c844e760f415dd3fb36e62befae4eba79d598c
Dana Murphy <danamurphy48@gmail.com>
2021-03-02 12:33:22 -0600

    Made static helper class

---
+++ DayTripGenerator/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayTripGenerator
+{
+    public static class Helper
+    {
+        public static int PickRandomNumber(int min, int max, Random random)
+        {
+            return random.Next(min, max);
+        }
+    }
+}
